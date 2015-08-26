9d2df0f677aaf078cde140aacac0364e4261748e
Stephen Dotz <stephen.dotz@nytimes.com>
2015-08-25 23:31:35 -0400

    impl drop

--- src/second.rs
+++ src/second.rs
@@ -5,48 +5,42 @@ struct Node {
 	next: Link,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
-
 struct List {
 	head: Link
 }
 
+type Link = Option<Box<Node>>;
+
 impl List {
 	pub fn new() -> Self {
-		List {head: Link::Empty}
+		List {head: None}
 	}
 
 	pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
         	elem: elem,
-        	next: mem::replace(&mut self.head, Link::Empty)
+        	next: mem::replace(&mut self.head, None)
         });
 
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-		match mem::replace(&mut self.head, Link::Empty) {
-			Link::Empty => None,
-			Link::More(boxed_node) => {
-				let node = *boxed_node;
-				self.head = node.next;
-				Some(node.elem)
-			}
-		}
-	}
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.elem
+        })
+    }
 
 }
 
 impl Drop for List {
 	fn drop(&mut self) {
-		let mut curr_link = mem::replace(&mut self.head, Link::Empty);
+		let mut curr_link = self.head.take();
 
-		while let Link::More(mut boxed_node) = curr_link {
-			curr_link = mem::replace(&mut boxed_node.next, Link::Empty)
+		while let Some(mut boxed_node) = curr_link {
+			curr_link = mem::replace(&mut boxed_node.next, None)
 		}
 	}
 }
