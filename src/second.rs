d53808b6996926edf5d37b0d29035dffb20c6aab
Stephen Dotz <stephen.dotz@nytimes.com>
2015-08-25 23:47:38 -0400

    peek and peek_mut

--- src/second.rs
+++ src/second.rs
@@ -1,22 +1,22 @@
 use std::mem;
 
-struct Node {
-	elem: i32,
-	next: Link,
+struct Node<T> {
+	elem: T,
+	next: Link<T>,
 }
 
-struct List {
-	head: Link
+pub struct List<T> {
+	head: Link<T>
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-impl List {
+impl<T> List<T> {
 	pub fn new() -> Self {
 		List {head: None}
 	}
 
-	pub fn push(&mut self, elem: i32) {
+	pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
         	elem: elem,
         	next: mem::replace(&mut self.head, None)
@@ -25,7 +25,7 @@ impl List {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             let node = *node;
             self.head = node.next;
@@ -33,9 +33,21 @@ impl List {
         })
     }
 
+    pub fn peek(&mut self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
 	fn drop(&mut self) {
 		let mut curr_link = self.head.take();
 
@@ -76,5 +88,20 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+
+    fn peek() {
+        let mut list = List::new();
+
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
     }
 }
