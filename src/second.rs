301128ae8869257aac174a6f5afac9d2ebb82e05
Stephen Dotz <stephen.dotz@nytimes.com>
2015-08-26 22:19:57 -0400

    iter

--- src/second.rs
+++ src/second.rs
@@ -11,12 +11,32 @@ pub struct List<T> {
 
 pub struct IntoIter<T>(List<T>);
 
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter<'a>(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
 type Link<T> = Option<Box<Node<T>>>;
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
@@ -128,5 +148,14 @@ mod test {
 
     }
 
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
 
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
 }
